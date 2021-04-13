a0402299d9cdf34b55285ab2cfcac2670fb2fd7c
Eslim Daga <esliim9811@gmail.com>
2021-04-13 11:37:54 -0500

    Add bodyparser and routes

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const app = express();
+
 //Configurar .env a ./config/config.env
 require("dotenv").config({
   path : "./config/config.env"
 });
 
-const app = express();
+//Usar BodyParser
+app.use(bodyParser.json());
+/* app.use(bodyParser.urlencoded({
+  extended: true
+})); */
+
 
 //Configurar solo para el desarrollo
 if(process.env.NODE_ENV === "development"){
@@ -20,6 +27,12 @@ if(process.env.NODE_ENV === "development"){
   //Morgan da información sobre cada petición
   //Cors está permitido lidiar con React para localhost al puerto 3000 sin ningun problema
 }
+
+//Cargar todas las rutas
+const authRouter = require("./routes/auth.route");
+
+//Usar rutas
+app.use("/api/",authRouter);
 
 app.use((req,res,next) => {
   res.status(404).json({
