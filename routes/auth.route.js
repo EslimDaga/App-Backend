df87e133295c787b5b60e6de2b6d590728760bad
Eslim Daga <esliim9811@gmail.com>
2021-04-13 17:08:47 -0500

    Add validation controller validation register

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-//Load Controllers
-const { registerController } = require("../controllers/auth.controller");
+//Validations
+const { validRegister,validLogin,forgotPasswordValidator,resertPasswordValidator } = require("../helpers/valid");
 
-router.post("/register", registerController);
+//Load Controllers
+const { registerController,activationController } = require("../controllers/auth.controller");
+
+router.post("/register",validRegister,registerController);
+router.post("/activation",activationController);
 
 module.exports = router;
