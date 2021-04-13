e71133aa71bda9451a984efcadddc8aa00aafd72
Eslim Daga <esliim9811@gmail.com>
2021-04-13 10:28:16 -0500

    Add config only development and cors

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ require("dotenv").config({
 
 const app = express();
 
+//Configurar solo para el desarrollo
+if(process.env.NODE_ENV === "development"){
+  app.use(cors({
+    origin : process.env.CLIENT_URL
+  }));
+
+  app.use(morgan("dev"));
+  //Morgan da información sobre cada petición
+  //Cors está permitido lidiar con React para localhost al puerto 3000 sin ningun problema
+}
+
+app.use((req,res,next) => {
+  res.status(404).json({
+    success : false,
+    message : "Page not Founded"
+  });
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT,() => {
